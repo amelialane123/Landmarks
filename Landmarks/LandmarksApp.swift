cc2a6e847ed999930efd9c119b904ca0debb96b9
Amelia Lane <amelia.lane@duke.edu>
2023-10-23 11:06:37 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Amelia Lane on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
